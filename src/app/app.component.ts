8a40016aa85ab323f278eedd6b5b3d5178c81cc3
Pedro Brandes <psbrandes@gmail.com>
2024-04-02 19:33:14 -0300

    Header finalizado, começando sessão banner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./componentes/header/header.component";
+import { BannerComponent } from "./componentes/banner/banner.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HeaderComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, BannerComponent]
 })
 export class AppComponent {
   title = 'PayWise';
