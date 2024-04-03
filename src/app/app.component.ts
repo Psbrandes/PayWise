f9ebcfab32c59158e3f78568549ea5f0e4a9908f
Pedro Brandes <psbrandes@gmail.com>
2024-04-03 15:51:38 -0300

    Pequenos ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,14 @@ import { BannerComponent } from "./componentes/banner/banner.component";
 import { AboutComponent } from "./componentes/about/about.component";
 import { LogoSliderComponent } from "./componentes/logo-slider/logo-slider.component";
 import { FeaturesComponent } from "./componentes/features/features.component";
+import { PagcardComponent } from "./componentes/pagcard/pagcard.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HeaderComponent, BannerComponent, AboutComponent, LogoSliderComponent, FeaturesComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, BannerComponent, AboutComponent, LogoSliderComponent, FeaturesComponent, PagcardComponent]
 })
 export class AppComponent {
   title = 'PayWise';
