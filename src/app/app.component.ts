9961e6a4c6688faadeeb3b5dddfb02fbe5c7b4ef
Pedro Brandes <psbrandes@gmail.com>
2024-04-02 23:18:16 -0300

    Componentes Banner e about prontos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./componentes/header/header.component";
 import { BannerComponent } from "./componentes/banner/banner.component";
+import { AboutComponent } from "./componentes/about/about.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HeaderComponent, BannerComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, BannerComponent, AboutComponent]
 })
 export class AppComponent {
   title = 'PayWise';
